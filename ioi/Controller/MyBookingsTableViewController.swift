34d3f38efd034bec7bbc16866e24a23d4a890460
Shlok97 <shlok.kapoor97@gmail.com>
2018-06-28 23:39:17 +0530

    Bookings Page

---
+++ ioi/Controller/MyBookingsTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MyBookingsTableViewController.swift
+//  ioi
+//
+//  Created by Shlok Kapoor on 28/06/18.
+//  Copyright © 2018 Shlok. All rights reserved.
+//
+
+import UIKit
+import FoldingCell
+
+class MyBookingsTableViewController: UITableViewController {
+    enum Const {
+        static let closeCellHeight: CGFloat = 150
+        static let openCellHeight: CGFloat = 340
+        static let rowsCount = 10
+    }
+    
+    var cellHeights: [CGFloat] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        
+        tableView.reloadData()
+    }
+    
+    private func setup() {
+        cellHeights = Array(repeating: Const.closeCellHeight, count: Const.rowsCount)
+        tableView.estimatedRowHeight = Const.closeCellHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        tableView.backgroundColor = UIColor(hexString: "#E9E9E9")
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = UIRefreshControl()
+            tableView.refreshControl?.addTarget(self, action: #selector(refreshHandler), for: .valueChanged)
+        }
+        
+        tableView.register(UINib(nibName: "BookingFoldingTableViewCell", bundle: nil), forCellReuseIdentifier: "bookingFoldingCell")
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+    }
+    
+    @objc func refreshHandler() {
+        let deadlineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { [weak self] in
+            if #available(iOS 10.0, *) {
+                self?.tableView.refreshControl?.endRefreshing()
+            }
+            self?.tableView.reloadData()
+        })
+    }
+}
+
+// MARK:- TableView Setup
+extension MyBookingsTableViewController {
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard case let cell as BookingFoldingTableViewCell = cell else {
+            return
+        }
+        
+        cell.backgroundColor = .clear
+        
+        if cellHeights[indexPath.row] == Const.closeCellHeight {
+            cell.unfold(false, animated: false, completion: nil)
+        } else {
+            cell.unfold(true, animated: false, completion: nil)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bookingFoldingCell", for: indexPath) as! BookingFoldingTableViewCell
+        let durations: [TimeInterval] = [0.2, 0.18, 0.18]
+        cell.durationsForExpandedState = durations
+        cell.durationsForCollapsedState = durations
+        return cell
+    }
+    
+    override func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeights[indexPath.row]
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
+        
+        if cell.isAnimating() {
+            return
+        }
+        
+        var duration = 0.0
+        let cellIsCollapsed = cellHeights[indexPath.row] == Const.closeCellHeight
+        if cellIsCollapsed {
+            cellHeights[indexPath.row] = Const.openCellHeight
+            cell.unfold(true, animated: true, completion: nil)
+            duration = 0.5
+        } else {
+            cellHeights[indexPath.row] = Const.closeCellHeight
+            cell.unfold(false, animated: true, completion: nil)
+            duration = 0.8
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }, completion: nil)
+    }
+}
---
+++ ioi/View/TableViewCell/BookingFoldingCell/BookingFoldingTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  BookingFoldingTableViewCell.swift
+//  ioi
+//
+//  Created by Shlok Kapoor on 28/06/18.
+//  Copyright © 2018 Shlok. All rights reserved.
+//
+
+import UIKit
+import FoldingCell
+
+class BookingFoldingTableViewCell: FoldingCell {
+
+    @IBOutlet var bookingImageView: UIImageView!
+    @IBOutlet var photographerImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.clipsToBounds = true
+        containerView.layer.cornerRadius = 10
+        containerView.clipsToBounds = true
+        
+        photographerImageView.randomImage()
+        bookingImageView.randomImage()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
