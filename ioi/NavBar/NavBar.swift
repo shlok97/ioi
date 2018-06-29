02bc88386085aaf349be06159c235581b9269f47
Shlok97 <shlok.kapoor97@gmail.com>
2018-06-29 13:09:37 +0530

    using navigation controller

---
+++ ioi/NavBar/NavBar.swift
@@ -0,0 +1,16 @@
+//
+//  NavBar.swift
+//  ioi
+//
+//  Created by Shlok Kapoor on 29/06/18.
+//  Copyright © 2018 Shlok. All rights reserved.
+//
+
+import UIKit
+
+class NavBar: UINavigationBar {
+    override func awakeFromNib() {
+        self.tintColor = UIColor.white
+        
+    }
+}
